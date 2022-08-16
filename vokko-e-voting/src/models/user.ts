c0d24fc8ab91604d0ba30f5bf9c77f5f6cf0bf0c
wraaflaub <walter.raaflaub@polypoint.ch>
2022-08-16 17:42:37 +0200

    Initiale Projektstruktur

---
+++ vokko-e-voting/src/models/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+    uuid: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    phoneNumber: string;
+    createdAt: Date;
+    modifiedAt: Date;
+}
