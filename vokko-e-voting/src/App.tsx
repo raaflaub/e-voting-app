b2b89b4261a7106cec1f02bc4e8b8179056abcc5
Walter Raaflaub <106313040+raaflaub@users.noreply.github.com>
2022-09-04 21:31:18 +0200

    vokko-Logo :-)

--- vokko-e-voting/src/App.tsx
+++ vokko-e-voting/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import vokkoLogo from './vokkoLogo.png';
 import './App.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -9,20 +9,9 @@ import '@fontsource/roboto/700.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <img src={vokkoLogo} alt="VOKKO logo" />
+        </header>
     </div>
   );
 }
