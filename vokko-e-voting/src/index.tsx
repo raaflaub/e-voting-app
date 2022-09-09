9515dc35fc6faf61d54afdecfc530caae14f2d9c
Walter Raaflaub <106313040+raaflaub@users.noreply.github.com>
2022-09-09 22:10:18 +0200

    Update index.tsx
    Routing-Grundstruktur

--- vokko-e-voting/src/index.tsx
+++ vokko-e-voting/src/index.tsx
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Expenses from "./testing-area/routes/expenses";
-import Invoices from "./testing-area/routes/invoices";
-import Invoice from "./testing-area/routes/invoice";
-import MyMuiTest from "./testing-area/my-mui-test";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
