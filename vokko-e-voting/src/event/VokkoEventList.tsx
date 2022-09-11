d62730a98354f7716f42a1b593c5bf12b062beb8
Walter Raaflaub <106313040+raaflaub@users.noreply.github.com>
2022-09-11 21:12:12 +0200

    VokkoEventList mit Paper

--- vokko-e-voting/src/event/VokkoEventList.tsx
+++ vokko-e-voting/src/event/VokkoEventList.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import {Stack} from "@mui/material";
+import {Button, Paper, Stack, Typography} from "@mui/material";
 import {Event} from '../model/vokkoEvents';
 
 export type VokkoEventListProps = { title: string, events: Event[], onDetail?: (event: Event) => void, onJoin?: (event: Event) => void }
 
 export default function VokkoEventList({ title, events, onDetail, onJoin }: VokkoEventListProps) {
     return (
+        <>
         <Stack>
+            <Typography variant="h5"> {title} </Typography>
             { events.map(
-                event => <div key={event.id}> {event.title} </div>
+                event =>
+                    <Paper key={event.id} style={{height:75, width:"100%", }}
+                           onClick={e => { if (onDetail) onDetail(event);} }>
+                        <Typography variant="h6"> {event.title} </Typography>
+                        { onJoin && <Button onClick={ e => onJoin(event) }>Teilnehmen</Button>}
+                    </Paper>
             ) }
         </Stack>
+        </>
     );
 }
