10988ccaf262ecea906bcc246fb0222f36811581
Walter Raaflaub <106313040+raaflaub@users.noreply.github.com>
2022-09-11 14:52:58 +0200

    vokko Header: Profile-Menu

--- vokko-e-voting/src/header/VokkoHeader.tsx
+++ vokko-e-voting/src/header/VokkoHeader.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {AppBar, IconButton, Toolbar, Typography} from "@mui/material";
+import React, {useState} from 'react';
+import {AppBar, Box, IconButton, Menu, MenuItem, Toolbar, Typography} from "@mui/material";
 import { ArrowBackIos, AccountCircle }  from "@mui/icons-material";
 import vokkoLogoSmall from "./vokkoLogoSmall.png";
 import {useNavigate} from "react-router-dom";
@@ -8,13 +8,25 @@ import {currentUser} from "../model/vokkoUsers";
 export type VokkoHeaderProps = { title: string, backButton: boolean, userProfile: boolean }
 
 export default function VokkoHeader( { title, backButton, userProfile } : VokkoHeaderProps) {
+
     const navigate = useNavigate();
+
+    const [anchorElProfile, setAnchorElProfile] = useState<null | HTMLElement>(null);
+
+    const handleOpenProfileMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElProfile(event.currentTarget);
+    };
+
+    const handleCloseProfileMenu = () => {
+        setAnchorElProfile(null);
+    };
+
     return (
         <AppBar position="sticky">
             <Toolbar>
 
                 { backButton &&
-                    <IconButton color="inherit" onClick={() => navigate(-1)}>
+                    <IconButton color="inherit" sx={{ p:0 }} onClick={() => navigate(-1)}>
                         <ArrowBackIos />
                     </IconButton>
                 }
@@ -35,12 +47,39 @@ export default function VokkoHeader( { title, backButton, userProfile } : VokkoH
 
                 { userProfile &&
                     <>
-                    <Typography variant="h6" align="right" sx={{ display: { xs: 'none', sm: 'flex' }, mr: 1 }} >
-                        { currentUser.email }
-                    </Typography>
-                    <IconButton color="inherit" onClick={() => navigate(-1)}>
-                        <AccountCircle />
-                    </IconButton>
+                        <Box sx={{ display: { xs: 'none', sm: 'flex' } }} >
+                            <Typography variant="h6" align="right">
+                                { currentUser.email }
+                            </Typography>
+                            <IconButton color="inherit" sx={{ pt:0, pb: 0, pr: 0 }}>
+                                <AccountCircle />
+                            </IconButton>
+                        </Box>
+                        <Box sx={{ display: { xs: 'flex', sm: 'none' } }} >
+                            <IconButton color="inherit" sx={{ p:0 }} onClick={handleOpenProfileMenu}>
+                                <AccountCircle />{/*  TODO: use avatar instead: <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />  */}
+                            </IconButton>
+                            <Menu
+                                sx={{ mt: '45px' }}
+                                id="menu-appbar"
+                                anchorEl={anchorElProfile}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorElProfile)}
+                                onClose={handleCloseProfileMenu}
+                            >
+                                <MenuItem disabled>
+                                    { currentUser.email }
+                                </MenuItem>
+                            </Menu>
+                        </Box>
                     </>
                 }
 
