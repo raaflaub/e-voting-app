07a5114271def707bb5ec6c8d2a1d5bb108a774f
Walter Raaflaub <106313040+raaflaub@users.noreply.github.com>
2022-09-12 13:44:49 +0200

    Motion, VotingOptions

--- vokko-e-voting/src/model/vokkoEvents.ts
+++ vokko-e-voting/src/model/vokkoEvents.ts
@@ -1,9 +1,10 @@
 // https://api.vokko.cloud/swagger/index.html
 
-export interface Event  {
+export interface Event {
     id:	string;
     eventDateAndTime: Date;
     title: string | null;
+    motions: Motion[];
 }
 
 export function isFutureEvent(e: Event): boolean {
@@ -14,16 +15,31 @@ export function isPastEvent(e: Event): boolean {
     return (new Date(e.eventDateAndTime.getTime() + 7_200_000) <= new Date());
 }
 
+export interface Motion {
+    id:	string;
+    votingTitle: string;
+    description: string | null;
+    ownerId: string | null;
+    startDate: Date;
+    endDate: Date;
+    timeout: number;
+    options: VotingOption[];
+}
+
+export interface VotingOption {
+    votingOptionId: string;
+    title: string;
+}
+
 export const allEvents: Event[] = [
     {
         "id": "b3d3c93c-dac3-4a7a-8b8a-1a219ed46b3d",
-        "eventDateAndTime": new Date(2022, 8, 11, 20, 30), //("2022-09-09T20:30:05.8834731+00:00"),
+        "eventDateAndTime": new Date(2022, 8, 12, 17, 30), //("2022-09-09T20:30:05.8834731+00:00"),
         "title": "HEC Alumni GeneralMeeting 2022",
-/*
-      "motions": [
+        "motions": [
             {
                 "id": "db961a27-f4df-4faa-9f9d-90fa0686d289",
-                "endDate": "2022-09-09T20:32:05.8834731+00:00",
+                "endDate": new Date(2022,9,9,20,32,5),
                 "options": [
                     {
                         "title": "Yes",
@@ -39,14 +55,14 @@ export const allEvents: Event[] = [
                     }
                 ],
                 "ownerId": null,
-                "startDate": "2022-09-09T20:30:05.8834731+00:00",
+                "startDate": new Date(2022,9,9,20,30,5),
                 "votingTitle": "Minutes 2021",
                 "timeout": 0,
                 "description": null
             },
             {
                 "id": "db961a27-f4df-4faa-9f9d-90fa0686d289",
-                "endDate": "2022-09-09T20:34:05.8834731+00:00",
+                "endDate": new Date(2022,9,9,20,34,5),
                 "options": [
                     {
                         "title": "Yes",
@@ -62,14 +78,14 @@ export const allEvents: Event[] = [
                     }
                 ],
                 "ownerId": null,
-                "startDate": "2022-09-09T20:32:05.8834731+00:00",
+                "startDate": new Date(2022,9,9,20,32,5),
                 "votingTitle": "Statement 2021",
                 "timeout": 0,
                 "description": null
             },
             {
                 "id": "db961a27-f4df-4faa-9f9d-90fa0686d289",
-                "endDate": "2022-09-09T20:36:05.8834731+00:00",
+                "endDate": new Date(2022,9,9,20,20,36,5),
                 "options": [
                     {
                         "title": "Yes",
@@ -85,14 +101,14 @@ export const allEvents: Event[] = [
                     }
                 ],
                 "ownerId": null,
-                "startDate": "2022-09-09T20:34:05.8834731+00:00",
+                "startDate": new Date(2022,9,9,20,34,5),
                 "votingTitle": "Budget 2023",
                 "timeout": 0,
                 "description": null
             },
             {
                 "id": "db961a27-f4df-4faa-9f9d-90fa0686d289",
-                "endDate": "2022-09-09T20:38:05.8834731+00:00",
+                "endDate": new Date(2022,9,9,20,38,5),
                 "options": [
                     {
                         "title": "Eric Olombel",
@@ -105,20 +121,45 @@ export const allEvents: Event[] = [
                     {
                         "title": "Walter Raaflaub",
                         "votingOptionId": "afc6b92e-fb0e-472c-952b-6e37e84b108f"
-                    }
+                    },
                 ],
                 "ownerId": null,
-                "startDate": "2022-09-09T20:36:05.8834731+00:00",
+                "startDate": new Date(2022,9,9,20,36,5),
                 "votingTitle": "Presidential Election",
                 "timeout": 0,
                 "description": null
             }
-        ]  */
+        ]
     },
     {
         "id": "ffd3c93c-dac3-4a7a-8b8a-1a219ed46b3f",
         "eventDateAndTime": new Date(2021, 9, 9, 20, 30), //("2022-09-09T20:30:05.8834731+00:00"),
-        "title": "HEC Alumni GeneralMeeting 2021"
+        "title": "HEC Alumni GeneralMeeting 2021",
+        "motions": [
+            {
+                "id": "db961a27-f4df-4faa-9f9d-90fa0686d289",
+                "endDate": new Date(2022,9,9,20,38,5),
+                "options": [
+                    {
+                        "title": "Eric Olombel",
+                        "votingOptionId": "fbefdb02-f1bb-4fde-af2a-9a09ecf16752"
+                    },
+                    {
+                        "title": "Jonathan Rejaud",
+                        "votingOptionId": "f5483519-a830-4e68-ad25-0e8b9594677e"
+                    },
+                    {
+                        "title": "Walter Raaflaub",
+                        "votingOptionId": "afc6b92e-fb0e-472c-952b-6e37e84b108f"
+                    },
+                ],
+                "ownerId": null,
+                "startDate": new Date(2022,9,9,20,36,5),
+                "votingTitle": "Presidential Election",
+                "timeout": 0,
+                "description": null
+            }
+        ]
     }
 ];
 
