dd05860fda92844a02cf816c6dd5d799ea1750bf
wraaflaub <walter.raaflaub@polypoint.ch>
2022-09-16 20:39:29 +0200

    Local Storage

--- vokko-e-voting/src/api/localstorage.ts
+++ vokko-e-voting/src/api/localstorage.ts
@@ -1,13 +1,49 @@
 import {useEffect, useState} from "react";
 
+export interface LocalStorageAPI<T> {
+    value: T|null;
+    setValue: (value: T) => void;
+    loading: boolean;
+}
 
-export function useLocallyStoredState<T>( key: string, defaultValue: T ) {
+export function useLocallyStoredState<T>( key: string ): LocalStorageAPI<T> {
 
-    const [ value, setValue ] = useState<T>(defaultValue);
+    const [ storageObj, setStorageObj ] = useState<{value:T|null, loading: boolean}>({ value: null, loading: true});
 
     useEffect(() => {
-        window.localStorage.setItem(key, JSON.stringify(value));
-    }, [value]);
+        let parsedValue = null;
+        const storedValue = window.localStorage.getItem(key);
+        console.log(key, 'initial read', storedValue)
+        if (storedValue) {
+            try {
+                parsedValue = JSON.parse(storedValue);
+                console.log(key, 'initial parsedValue', JSON.stringify(parsedValue));
+                setStorageObj({value: parsedValue, loading: false});
+            } catch(e) {
+                window.localStorage.removeItem(key);
+                console.log(key, 'initial parsedValue exception', e);
+                setStorageObj({value: null, loading: false});
+            }
+        } else {
+            setStorageObj({value: null, loading: false});
+        }
+    }, []);
 
-    return [ value, setValue ];
+    useEffect(() => {
+        if (storageObj?.value !== null) {
+            window.localStorage.setItem(key, JSON.stringify(storageObj?.value));
+            console.log(key, 'stored', JSON.stringify(storageObj?.value));
+        } else {
+            window.localStorage.removeItem(key);
+            console.log(key, 'cleared');
+        }
+    }, [storageObj]);
+
+
+    console.log('value=',JSON.stringify(storageObj.value), 'loading=',storageObj.loading);
+    return {
+        value: storageObj.value,
+        setValue: (value) => setStorageObj({value, loading: false}),
+        loading: storageObj.loading
+    };
 }
