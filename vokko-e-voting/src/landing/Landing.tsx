7b376078953a95294d683cba2cb12415c145b09d
wraaflaub <walter.raaflaub@polypoint.ch>
2022-09-18 10:20:53 +0200

    Landing Page: als Organizer registrieren, Unterscheidung Mobile/Desktop

--- vokko-e-voting/src/landing/Landing.tsx
+++ vokko-e-voting/src/landing/Landing.tsx
@@ -1,33 +1,73 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import {Outlet, useNavigate} from "react-router-dom";
-import {Button, Container, Grid} from "@mui/material";
+import {Button, ButtonGroup, Container, Grid, Typography} from "@mui/material";
 import VokkoHeader from "../header/VokkoHeader";
+import {UserContext} from "../provider/UserContextProvider";
+import {HubContext} from "../provider/HubContextProvider";
+import CategoryTitle from "../layout/CategoryTitle";
 
 export default function Landing() {
+
+    const isDevEnvironment = (process.env.NODE_ENV === 'development');
+    const isMobileEnvironment = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+    const hub = useContext(HubContext);
+    const user = useContext(UserContext);
     const navigate = useNavigate();
+
+    // Sobald Hub-Verbindung steht und falls der User korrekt registriert ist:
+    // Weiterleiten zur Voter-Event-Uebersicht.
+
+    const defaultAction = () => {
+        if (hub && user.value?.user?.userId) {
+            navigate('/voter');
+        }
+    }
+
+    useEffect(() => {
+        if (process.env.NODE_ENV !== 'development') {
+            defaultAction();
+        }
+    }, [hub, user]);
+
     return (
         <>
             <VokkoHeader title=" " backButton={false} userProfile={false} />
             <Container maxWidth="sm">
 
-                <p><em>(Hier aufgrund Benutzertyp automatisch verzweigen zu Registrierung / weiter als Wähler / weiter als Organisator)</em></p>
+                <CategoryTitle>
+                    Willkommen
+                </CategoryTitle>
 
-                <Grid container spacing={2} justifyContent="center">
-                    <Grid item>
-                        <Button variant="contained" onClick={() => navigate("join/2?lastname=Spring&firstname=Peter&email=peter.spring@students.bfh.ch")}>Registrieren</Button>
-                    </Grid>
-                    <Grid item>
-                        <Button variant="contained" onClick={() => navigate("organizer")}>Ein Meeting organisieren</Button>
-                    </Grid>
-                    <Grid item>
-                        <Button variant="contained" onClick={() => navigate("voter")}>An einem Meeting teilnehmen</Button>
-                    </Grid>
-                    {/*    <Grid item>
-                        <Button variant="contained" onClick={() => navigate("test")}>Test-Area</Button>
-                    </Grid>  */}
-                </Grid>
+                <Typography variant="h5" color="text.secondary" sx={{ mt: 6, mb: 1}} >Falls du eine Event-Einladung erhalten hast</Typography>
+                <Typography variant="subtitle1" >Bitte klicke auf den Link «TEILNEHMEN» im Einladungs-Mail.</Typography>
 
-                <Outlet />
+                <Typography variant="h5" color="text.secondary" sx={{ mt: 6, mb: 1}} >Falls du einen Event organisieren möchtest</Typography>
+                {
+                    isMobileEnvironment &&
+                    <Typography variant="subtitle1"  sx={{ mb: 2}} >Bitte öffne «https://vokko.cloud» im Browser auf dem Desktop. {isMobileEnvironment}</Typography>
+                }
+                {
+                    !isMobileEnvironment &&
+                    <>
+                        <Typography variant="subtitle1"  sx={{ mb: 2}} >Bitte registriere dich:</Typography>
+                        <Button
+                            variant="outlined"
+                            onClick={() => navigate("join/2?lastname=Organizer&firstname=The&email=organizer@vokko.cloud&view=organizer")}>
+                            Als Organisator registrieren
+                        </Button>
+                    </>
+
+                }
+
+                {isDevEnvironment && <Container sx={{ mx:0, my:8 }}>
+                    <CategoryTitle>Dev section</CategoryTitle>
+                    <ButtonGroup variant="outlined">
+                        <Button onClick={defaultAction}>Default action</Button>
+                        <Button onClick={() => navigate("organizer")}>Organizer View</Button>
+                    </ButtonGroup>
+                </Container>
+                }
             </Container>
         </>
     );
