27002e339560fe69d8671f2ce84e6b858c54e85f
wraaflaub <walter.raaflaub@polypoint.ch>
2022-09-18 11:42:25 +0200

    Local Storage: Log-Meldungen entfernt

--- vokko-e-voting/src/api/localstorage.ts
+++ vokko-e-voting/src/api/localstorage.ts
@@ -13,15 +13,12 @@ export function useLocallyStoredState<T>( key: string ): LocalStorageAPI<T> {
     useEffect(() => {
         let parsedValue = null;
         const storedValue = window.localStorage.getItem(key);
-        console.log('useLocallyStoredState', key, 'initial read', storedValue)
         if (storedValue) {
             try {
                 parsedValue = JSON.parse(storedValue);
-                console.log('useLocallyStoredState', key, 'initial parsedValue', JSON.stringify(parsedValue));
                 setStorageObj({value: parsedValue, loading: false});
             } catch(e) {
                 window.localStorage.removeItem(key);
-                console.log('useLocallyStoredState', key, 'initial parsedValue exception', e);
                 setStorageObj({value: null, loading: false});
             }
         } else {
@@ -35,19 +32,15 @@ export function useLocallyStoredState<T>( key: string ): LocalStorageAPI<T> {
                 const stringValue = JSON.stringify(storageObj?.value);
                 if (window.localStorage.getItem(key) !== stringValue) {
                     window.localStorage.setItem(key, stringValue);
-                    console.log('useLocallyStoredState', key, 'stored', stringValue);
                 }
             } else {
                 if (window.localStorage.getItem(key) !== null) {
                     window.localStorage.removeItem(key);
-                    console.log('useLocallyStoredState', key, 'cleared');
                 }
             }
         }
     }, [storageObj]);
 
-
-    console.log('useLocallyStoredState', key, 'value=',JSON.stringify(storageObj.value), 'loading=',storageObj.loading);
     return {
         value: storageObj.value,
         setValue: (value) => setStorageObj({value, loading: false}),
