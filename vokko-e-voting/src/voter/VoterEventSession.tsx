3213c1eefd906d239a42f823bcecfd514f0ba5f5
wraaflaub <walter.raaflaub@polypoint.ch>
2022-09-23 21:47:01 +0200

    VoteDialogs anwenden

--- vokko-e-voting/src/voter/VoterEventSession.tsx
+++ vokko-e-voting/src/voter/VoterEventSession.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import VokkoHeader from "../header/VokkoHeader";
 import {useParams} from "react-router-dom";
 import MotionList from "../motion/MotionList";
@@ -6,24 +6,41 @@ import {Container} from "@mui/material";
 import EventMonitorContextProvider from "../provider/EventMonitorContextProvider";
 import EventStatusBar from "../event/EventStatusBar";
 import {useEvent} from "../api/persistence";
+import VoteDialogs from "../vote/VoteDialogs";
+import {IVoting} from "../api/model/ivoting";
+import {useVotingDialogState, useVotingStartEndNotifications} from "../vote/votingStartedEndedNotifications";
 
 export default function VoterEventSession() {
 
     const params = useParams();
     const { event }  = useEvent(params.eventId!);
 
+    const [ dialogState, setDialogState ] = useVotingDialogState();
+    useVotingStartEndNotifications(dialogState, setDialogState);
+
+    const doOnAction = (motion: IVoting) => {
+        if (motion.id) {
+            setDialogState({...dialogState, visibleDialog: 'SHOW_PREVIEW', motionId: motion.id});
+        }
+    }
+
     return (
         <>
             <VokkoHeader title={event?.title} />
             {
                 event &&
-                <EventMonitorContextProvider eventId={event.id! /*"b3d3c93c-dac3-4a7a-8b8a-1a219ed46b3d"*/}>
+                <EventMonitorContextProvider eventId={event.id!}>
                     <Container maxWidth="md">
                         <EventStatusBar/>
-                        <MotionList motions={event.motions!}/>
+                        <MotionList motions={event!.motions!} actionTitle="Preview" onAction={doOnAction}/>
                     </Container>
+                    {
+                        event &&
+                        <VoteDialogs event={event} dialogState={dialogState} setDialogState={setDialogState} />
+                    }
                 </EventMonitorContextProvider>
             }
         </>
     );
 }
+
