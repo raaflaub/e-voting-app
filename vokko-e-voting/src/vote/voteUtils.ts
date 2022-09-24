6a51e38caf60d74924a486a251a500c81888c9ae
wraaflaub <walter.raaflaub@polypoint.ch>
2022-09-24 15:05:23 +0200

    voteUtils

---
+++ vokko-e-voting/src/vote/voteUtils.ts
@@ -0,0 +1,32 @@
+import {VotingOption} from "../api/model/voting-option";
+
+export type VotingState = 'PENDING' | 'INPROGRESS' | 'COMPLETED' | 'ACCEPTED' | 'REJECTED' | 'TIE';
+
+export function isYes(votingOption: VotingOption) {
+    return votingOption.title && ['YES','JA'].includes(votingOption.title?.toUpperCase());
+}
+
+export function isNo(votingOption: VotingOption) {
+    return votingOption.title && ['NO','NEIN'].includes(votingOption.title?.toUpperCase());
+}
+
+export function isYesNoVote(options: VotingOption[] | null) {
+    return options && options.find(isYes) && options.find(isNo);
+}
+
+export function getVoteResultState(options: VotingOption[] | null) {
+    if (!options || !isYesNoVote(options)) {
+        return 'COMPLETED'
+    }
+
+    const voteCountBalance = options.reduce(
+        (sum, votingOption) =>
+            votingOption.voteCount === undefined ? sum
+            : isYes(votingOption)                ? sum + votingOption.voteCount
+            : isNo(votingOption)                 ? sum - votingOption.voteCount
+            :                                      sum,
+        0
+    );
+
+    return voteCountBalance > 0? 'ACCEPTED' : voteCountBalance < 0? 'REJECTED' : 'TIE';
+}
