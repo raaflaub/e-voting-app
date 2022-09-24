05cf56b56688b11d86939a3009eac348c6a503a3
wraaflaub <walter.raaflaub@polypoint.ch>
2022-09-24 10:12:24 +0200

    EventStatusBar testing

--- vokko-e-voting/src/event/EventStatusBar.tsx
+++ vokko-e-voting/src/event/EventStatusBar.tsx
@@ -1,12 +1,13 @@
 import {useContext} from 'react';
 import {EventMonitorContext} from "../provider/EventMonitorContextProvider";
 import {Typography} from "@mui/material";
+import {eventMonitorToString} from "../api/messaging";
 
 export default function EventStatusBar() {
     const eventMonitor = useContext(EventMonitorContext);
     return (
         <Typography>
-            {JSON.stringify(eventMonitor, null, 1)}
+            {eventMonitorToString(eventMonitor)}
         </Typography>
     );
 }
