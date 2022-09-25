7034a1a563310325836e12c21f13df1374dcc4d9
wraaflaub <walter.raaflaub@polypoint.ch>
2022-09-25 17:08:37 +0200

    useSignVote

---
+++ vokko-e-voting/src/vote/signVote.ts
@@ -0,0 +1,74 @@
+import {CastVoteRequestData} from "../api/model/cast-vote-request-data";
+import {RegisteredUser} from "../provider/UserContextProvider";
+import {IKeyPair} from "../criptography/IKeyPair";
+import {RsaProvider} from "../criptography/RsaProvider";
+import {useState} from "react";
+
+export interface SignVoteAPI {
+    signedVoteRequest: CastVoteRequestData | null,
+    isLoading: boolean,
+    isSuccess: boolean,
+    isError: boolean,
+    error: any
+    sign: (voteRequest: CastVoteRequestData) => void,
+}
+
+export function useSignVote(user: RegisteredUser): SignVoteAPI {
+
+    async function sign(voteRequest: CastVoteRequestData) {
+        console.log('### SignVoteAPI.sign', new Date().toISOString());
+        try {
+            console.log('### set isLoading');
+            setSignVoteApi((signVoteApi) => ({
+                ...signVoteApi, isLoading: true, isSuccess: false, isError: false, error: null, signedVoteRequest: null
+            }));
+
+            if (!user.user?.publicKey || !user.privateKey) {
+                console.error('### signVote: invalid keypair')
+                throw new Error("### invalid keypair");
+            }
+
+            const keyPair: IKeyPair = {
+                PublicKey: user.user?.publicKey,
+                PrivateKey: user.privateKey
+            };
+
+            const rsaProvider = new RsaProvider(window.crypto,new TextEncoder());
+
+            await rsaProvider.ImportKeyPair(keyPair);
+            console.log('### import keypair');
+
+            const payload = voteRequest.userId! + voteRequest.votingId! + voteRequest.optionId!
+            console.log('### payload', payload);
+
+            const signature = await rsaProvider.Sign(payload);
+            console.log('### SIGNATURE', signature);
+            console.log('### castVoteRequestData', JSON.stringify({...voteRequest, signature}));
+
+            setSignVoteApi((signVoteApi) => ({
+                ...signVoteApi, isLoading: false, isSuccess: true, isError: false, signedVoteRequest: {...voteRequest, signature}
+            }));
+            console.log('### set isSuccess');
+
+        } catch (e) {
+            console.error('### sign ERROR', e);
+            setSignVoteApi((signVoteApi) => ({
+                ...signVoteApi, isLoading: false, isSuccess: false, isError: true, error: e
+            }));
+        }
+    }
+
+    const INITIAL_SIGNVOTEAPI: SignVoteAPI = {
+        signedVoteRequest: null,
+        isLoading: false,
+        isSuccess: false,
+        isError: false,
+        error: null,
+        sign,
+    }
+
+    const [ signVoteApi, setSignVoteApi ] = useState<SignVoteAPI>(INITIAL_SIGNVOTEAPI);
+
+    return signVoteApi;
+}
+
