41baea774e002d1fbcdaee4c9fdd60472834154e
wraaflaub <walter.raaflaub@polypoint.ch>
2022-09-28 21:10:00 +0200

    EventStatusBar mit Linie

--- vokko-e-voting/src/event/EventStatusBar.tsx
+++ vokko-e-voting/src/event/EventStatusBar.tsx
@@ -9,11 +9,13 @@ export default function EventStatusBar() {
     const eventMonitor = useContext(EventMonitorContext);
     return (
 
-            <Box sx={{ display: 'flex',width: '100%', flexDirection:'column'  }}>
+            <Box sx={{ display: 'flex',width: '100%', flexDirection:'column',
+                borderBottom: 1, borderColor: 'divider'
+            }}>
                 <Box sx={{ display: 'flex', width: '100%', flexDirection:'row',justifyContent:"space-between", padding: "15px 0 0px 0px"  }}>
-            <Badge badgeContent={eventMonitor?.usersOnlineCount} color="success">
-                <PeopleIcon fontSize="large" />
-            </Badge>
+                    <Badge badgeContent={eventMonitor?.usersOnlineCount} color="success">
+                        <PeopleIcon fontSize="large" />
+                    </Badge>
                     <CircularProgress size={30} color="success" variant="determinate" value={eventMonitor?.motionsCompletedCount/eventMonitor?.motionsCount*100} />
 
                 </Box>
