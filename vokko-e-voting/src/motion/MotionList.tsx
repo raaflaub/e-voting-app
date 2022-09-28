68958a77583561e1d9738908a3aaf413a32135e4
wraaflaub <walter.raaflaub@polypoint.ch>
2022-09-28 21:31:49 +0200

    Header/Footer in MotionList

--- vokko-e-voting/src/motion/MotionList.tsx
+++ vokko-e-voting/src/motion/MotionList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 import {Stack} from "@mui/material";
 import MotionListItem, {MotionListItemProps} from "./MotionListItem";
 import {IVoting} from "../api/model/ivoting";
@@ -8,24 +8,34 @@ export type MotionListProps = {
     onPreview?: (motion: IVoting) => void;
     onVote?: (motion: IVoting) => void;
     onViewResults?: (motion: IVoting) => void;
+    header?: ReactNode;
+    footer?: ReactNode;
     disabled?: boolean;
 }
 
-export default function MotionList({ motions, onPreview, onVote, onViewResults, disabled }: MotionListProps) {
+export default function MotionList({ motions, onPreview, onVote, onViewResults, header, footer, disabled }: MotionListProps) {
     return (
         <>
         <Stack spacing={2} mb={5}>
-            { motions.map(
-                motion =>
-                    <MotionListItem
-                        key={motion.id}
-                        motion={motion}
-                        onPreview={onPreview}
-                        onVote={onVote}
-                        onViewResults={onViewResults}
-                        disabled={disabled}
-                    />
-            ) }
+            {
+                header
+            }
+            {
+                motions.map(
+                    motion =>
+                        <MotionListItem
+                            key={motion.id}
+                            motion={motion}
+                            onPreview={onPreview}
+                            onVote={onVote}
+                            onViewResults={onViewResults}
+                            disabled={disabled}
+                        />
+                )
+            }
+            {
+                footer
+            }
         </Stack>
         </>
     );
