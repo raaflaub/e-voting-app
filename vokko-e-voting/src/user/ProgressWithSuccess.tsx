3af76027996306dd34cbcd14bd9aee9fce93e251
wraaflaub <walter.raaflaub@polypoint.ch>
2022-09-29 15:19:08 +0200

    ProgressWithSuccess

---
+++ vokko-e-voting/src/user/ProgressWithSuccess.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Avatar, Box} from "@mui/material";
+import {blue} from "@mui/material/colors";
+import CheckIcon from "@mui/icons-material/Check";
+import CircularProgress from "@mui/material/CircularProgress";
+
+export type ProgressWithSuccess = {
+    loading: boolean;
+    success: boolean;
+}
+
+export default function ProgressWithSuccess({ loading, success }: ProgressWithSuccess) {
+    return (
+        <Box sx={{ position:'relative' }}>
+            <Avatar sx={{ bgcolor:loading? blue[50] : blue[700] }}>
+                {success && <CheckIcon />}
+            </Avatar>
+                {
+                    loading &&
+                    <CircularProgress
+                        sx={{
+                            color: blue[700],
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            zIndex: 1,
+                        }}
+                    />
+                }
+        </Box>
+    );
+}
