e80114f99b4223fb0f6079199a46b2a218f27491
wraaflaub <walter.raaflaub@polypoint.ch>
2022-10-01 14:34:04 +0200

    getWinningOptions, getVoteResultText

--- vokko-e-voting/src/vote/voteUtils.ts
+++ vokko-e-voting/src/vote/voteUtils.ts
@@ -1,22 +1,22 @@
 import {VotingOption} from "../api/model/voting-option";
 import {IVoting} from "../api/model/ivoting";
 
-export type VotingState = 'PENDING' | 'INPROGRESS' | 'COMPLETED' | 'ACCEPTED' | 'REJECTED' | 'TIE';
+type VotingState = 'PENDING' | 'IN_PROGRESS' | 'COMPLETED' | 'ACCEPTED' | 'REJECTED' | 'DRAW';
 
-export const VOTING_STATE_TEXTS_DE = {
-    'PENDING':    'Ausstehend',
-    'INPROGRESS': 'Läuft...',
-    'COMPLETED':  'Abgeschlossen',
-    'ACCEPTED':   'Angenommen',
-    'REJECTED':   'Abgelehnt',
-    'TIE':        'Stichentscheid',
+const VOTING_STATE_TEXTS_DE = {
+    'PENDING':     'Ausstehend',
+    'IN_PROGRESS': 'Läuft...',
+    'COMPLETED':   'Abgeschlossen',
+    'ACCEPTED':    'Angenommen',
+    'REJECTED':    'Abgelehnt',
+    'DRAW':        'Patt',
 };
 
-export function isYes(votingOption: VotingOption) {
+function isYes(votingOption: VotingOption) {
     return votingOption.title && ['YES','JA'].includes(votingOption.title?.toUpperCase());
 }
 
-export function isNo(votingOption: VotingOption) {
+function isNo(votingOption: VotingOption) {
     return votingOption.title && ['NO','NEIN'].includes(votingOption.title?.toUpperCase());
 }
 
@@ -24,11 +24,11 @@ export function isYesNoVote(options: VotingOption[] | null) {
     return options && options.find(isYes) && options.find(isNo);
 }
 
-export function hasVotes(options: VotingOption[] | null) {
+function hasVotes(options: VotingOption[] | null) {
     return options && options.find(option => (option.voteCount ?? 0) > 0);
 }
 
-export function isVoteInProgress(motion: IVoting | null): boolean {
+function isVoteInProgress(motion: IVoting | null): boolean {
     const currentTime = new Date().getTime();
     if (motion?.startDate && motion?.endDate) {
         return (motion?.startDate.getTime() < currentTime) && (motion?.endDate.getTime() > currentTime);
@@ -37,7 +37,7 @@ export function isVoteInProgress(motion: IVoting | null): boolean {
     }
 }
 
-export function isVoteCompleted(motion: IVoting | null): boolean {
+function isVoteCompleted(motion: IVoting | null): boolean {
     return motion?.endDate? (motion?.endDate.getTime() < new Date().getTime()): false;
 }
 
@@ -46,33 +46,63 @@ export function isEndDateWithinTimeout(motion: IVoting | null, timeOutMs: number
 }
 
 
-export function getVoteResultState(motion: IVoting | null) {
+export function getVoteResultState(motion: IVoting | null, numWinners: number = 1) {
 
     if (!motion?.options) {
         return 'PENDING';
     }
 
     if (isVoteInProgress(motion)) {
-        return 'INPROGRESS'
+        return 'IN_PROGRESS'
     }
 
     if (!isVoteCompleted(motion)) {
         return 'PENDING';
     }
 
-    if (!isYesNoVote(motion?.options)) {
-        return 'COMPLETED'
+    const winningOptions = getWinningOptions(motion?.options, numWinners);
+
+    if ((winningOptions.length !== numWinners) || (winningOptions.length === 0)) {
+        return 'DRAW';  // Es gibt ein Patt, ein Stichentscheid ist notwendig
+    } else if (!isYesNoVote(motion?.options)) {
+        return 'COMPLETED';
+    } else if (isYes(winningOptions[0])) {
+        return 'ACCEPTED';
+    } else {
+        return 'REJECTED';
     }
-
-    const voteCountBalance = motion?.options.reduce(
-        (sum, votingOption) =>
-            votingOption.voteCount === undefined ? sum
-            : isYes(votingOption)                ? sum + votingOption.voteCount
-            : isNo(votingOption)                 ? sum - votingOption.voteCount
-            :                                      sum,
-        0
-    );
-
-    return voteCountBalance > 0? 'ACCEPTED' : voteCountBalance < 0? 'REJECTED' : 'TIE';
 }
 
+/*export*/ function getWinningOptions(options: VotingOption[], numWinners: number = 1) {
+    const sortedOptions = options.sort(
+        (option1, option2) =>
+            Math.sign((option2.voteCount ?? 0) - (option1.voteCount ?? 0))
+    );
+
+    // <numWinners> ist die Anzahl zu besetzenden Positionen bzw. die Anzahl Gewinner der Wahl.
+    // Wenn mehrere Optionen dieselbe Stimmenzahl erreichen, kann es ein Patt geben, was einen Stichentscheid provoziert.
+    // Wir geben alle möglichen Gewinner zurück. Das Array wird in diesem Fall länger als <numWinners>.
+    if (sortedOptions.length > numWinners) {
+        const requiredVotesThreshold = sortedOptions[numWinners-1].voteCount ?? 0;
+        return sortedOptions.filter(
+            (option) => (option.voteCount ?? 0) >= requiredVotesThreshold
+        )
+    } else {
+        return sortedOptions;
+    }
+}
+
+/*export*/ function getWinningOptionsText(options: VotingOption[]) {
+    return getWinningOptions(options)
+        ?.map((votingOption: VotingOption) => votingOption.title)
+        .join(", ")
+        ?? "";
+}
+
+export function getVoteResultText(motion: IVoting) {
+    const voteResultState = getVoteResultState(motion);
+    
+    return voteResultState === 'COMPLETED' ? 
+        'Gewählt: ' + getWinningOptionsText(motion.options ?? [])
+        : VOTING_STATE_TEXTS_DE[voteResultState];
+}
