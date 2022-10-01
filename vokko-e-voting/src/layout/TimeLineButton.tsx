3ac54939e07bdb9e0470d734e55d17cd407757d0
wraaflaub <walter.raaflaub@polypoint.ch>
2022-10-01 20:25:06 +0200

    Property disabled ergänzt

--- vokko-e-voting/src/layout/TimeLineButton.tsx
+++ vokko-e-voting/src/layout/TimeLineButton.tsx
@@ -3,14 +3,15 @@ import {Box, Button} from "@mui/material";
 
 export type TimeLineButtonProps = {
     variant?: 'text' | 'outlined' | 'contained';
+    disabled?: boolean;
     onClick: () => void;
     children: ReactNode;
 }
 
-export default function TimeLineButton({ variant, onClick, children}: TimeLineButtonProps) {
+export default function TimeLineButton({ variant, disabled, onClick, children}: TimeLineButtonProps) {
     return (
         <Box sx={{ display: 'flex', width: '100%', flexDirection:'row',justifyContent:"center", py: 2  }}>
-            <Button variant={variant} onClick={() => onClick()}>
+            <Button variant={variant} disabled={disabled} onClick={() => onClick()}>
                 {children}
             </Button>
         </Box>
