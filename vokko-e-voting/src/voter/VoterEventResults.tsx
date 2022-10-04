5acb41ba5f65af80818393b7c87548c614fd8701
wraaflaub <walter.raaflaub@polypoint.ch>
2022-10-04 18:44:53 +0200

    gleiche Darstellung der Resultate bei Votern wie bei Organizer

--- vokko-e-voting/src/voter/VoterEventResults.tsx
+++ vokko-e-voting/src/voter/VoterEventResults.tsx
@@ -29,7 +29,7 @@ export default function VoterEventResults() {
                 }
                 {
                     Boolean(motionsWithResults?.length) &&
-                    <MotionList motions={motionsWithResults ?? []} />
+                    <OrganizerResultList motions={motionsWithResults ?? []} />
                 }
             </Container>
         </>
