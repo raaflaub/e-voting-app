41fcbf67c95698f2a0a7ff02d5e3da4e70fd2fb0
wraaflaub <walter.raaflaub@polypoint.ch>
2022-10-06 21:36:16 +0200

    Test-Code aus Landing-Page entfernt

--- vokko-e-voting/src/landing/Landing.tsx
+++ vokko-e-voting/src/landing/Landing.tsx
@@ -7,7 +7,6 @@ import {HubContext} from "../provider/HubContextProvider";
 import CategoryTitle from "../layout/CategoryTitle";
 import {IUser} from "../api/model/iuser";
 import {buildRelativeInvitationLink} from "../user/userRegistration";
-import {ResultBar} from "../results/ResultBar";
 import {useTranslation} from "react-i18next";
 
 
@@ -83,39 +82,10 @@ export default function Landing() {
                             const user: IUser = { lastName: randomId, firstName: "Voter", email: `voter.${randomId}@vokko.cloud`};
                             navigate(buildRelativeInvitationLink( "default", user));
                         }}>Register as voter</Button>
+                        <Button onClick={() => navigate("voter")}>Voter View</Button>
                         <Button onClick={() => navigate("organizer")}>Organizer View</Button>
                     </ButtonGroup>
-                    {/*<VoteProgress endDate={new Date(Date.now() + 120000)} />*/}
 
-
-                    <ResultBar  options={
-
-                        [
-                            {votingOptionId: "dsadasdadss-dasdsadsadassdas-dsasdasdsa", title: "Yes",voteCount: 10},
-                            {votingOptionId: "dsadasddsadas-dasdsadsadassdas-dsasdasdsa", title: "No", voteCount: 5},
-                            {votingOptionId: "dsadasddsa-dasdsadsadassdas-dsasdasdsa", title: "Abstain", voteCount: 2}
-                        ]
-
-                    }  />
-
-                    {/*<VoteOptionsControl  options={*/}
-
-                    {/*    [*/}
-                    {/*        {votingOptionId: "dsadasdadss-dasdsadsadassdas-dsasdasdsa", title: "Yes"},*/}
-                    {/*        {votingOptionId: "dsadasddsadas-dasdsadsadassdas-dsasdasdsa", title: "No"},*/}
-                    {/*        {votingOptionId: "dsadasddsa-dasdsadsadassdas-dsasdasdsa", title: "Abstain"}*/}
-                    {/*    ]*/}
-
-                    {/*} voteOptionCount={1} onSelectionChanged={console.log}/>*/}
-                    {/*<VoteOptionsControl options={*/}
-
-                    {/*    [*/}
-                    {/*        {votingOptionId: "dsadasdadss-dasdsadsadassdas-dsasdasdsa", title: "Walter"},*/}
-                    {/*        {votingOptionId: "dsadasddsadas-dasdsadsadassdas-dsasdasdsa", title: "Remo"},*/}
-                    {/*        {votingOptionId: "dsadasddsa-dasdsadsadassdas-dsasdasdsa", title: "Jonas"}*/}
-                    {/*    ]*/}
-
-                    {/*} voteOptionCount={2} onSelectionChanged={console.log} disabled={true}/>*/}
                 </Container>
 
                 }
